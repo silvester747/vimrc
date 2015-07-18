d9939d22c1cad4f45c182bbc194d03cd6cfa3597
Rob van der Most <Silvester747@gmail.com>
2015-07-18 16:41:39 +0200

    Add update script.

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo -H pip install --upgrade pip
+sudo -H pip install --upgrade powerline-status
+sudo -H pip install --upgrade jedi
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle/vim-wombat256mod
+git pull
+cd ~/.vim/bundle/ctrlp.vim
+git pull
+cd ~/.vim/bundle/jedi-vim
+git pull
+cd ~/.vim/bundle/nerdtree
+git pull
