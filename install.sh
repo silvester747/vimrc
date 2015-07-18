b523746ed5683ef2cda7e43f4acfe17f32bd0754
Rob van der Most <Silvester747@gmail.com>
2015-07-18 12:37:02 +0200

    Install pathogen.

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt-get install vim python-pip
 sudo -H pip install --upgrade pip
 sudo -H pip install powerline-status
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
