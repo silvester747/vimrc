9b91ee9475d26ae9c18f1fedccbff8db156282a5
Rob van der Most <silvester747@gmail.com>
2015-07-29 20:13:30 +0200

    Combine install and update script in one.

--- install.sh
+++ install.sh
@@ -2,13 +2,23 @@
 
 sudo apt-get install vim python-pip
 sudo -H pip install --upgrade pip
-sudo -H pip install powerline-status
-sudo -H pip install jedi
+sudo -H pip install --upgrade powerline-status
+sudo -H pip install --upgrade jedi
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-git clone https://github.com/michalbachowski/vim-wombat256mod.git ~/.vim/bundle/vim-wombat256mod
-git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+function clone_or_pull {
+  if [ -d "$1" ]; then
+    cd "$1"
+    git pull
+  else
+    git clone $2
+  fi
+}
+
+clone_or_pull ~/.vim/bundle/vim-wombat256mod https://github.com/michalbachowski/vim-wombat256mod.git
+clone_or_pull ~/.vim/bundle/ctrlp.vim https://github.com/kien/ctrlp.vim.git
+clone_or_pull ~/.vim/bundle/jedi-vim "--recursive https://github.com/davidhalter/jedi-vim.git"
+clone_or_pull ~/.vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git
+
